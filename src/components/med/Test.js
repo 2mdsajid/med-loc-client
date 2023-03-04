12834a54aa309fdf83fa132e3cbf02e6ccdf1dc6
2mdsajid <2mdsajid@gmail.com>
2023-03-04 20:31:38 +0545

    added tests ui

--- src/components/med/Test.js
+++ src/components/med/Test.js
@@ -2,19 +2,39 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 // import SubTestTitleDisplay from './re-comp/SubTestTitleDisplay'
 import ROOT from '../Const';
+import Header from './re-comp/Header'
+
+function TestButtonList({ tests, categoryFilter, onClick }) {
+  const filteredTests = tests.filter(test => test.category === categoryFilter)
+
+  if (filteredTests.length === 0) {
+    return null // Don't render anything if there are no tests for the category
+  }
+
+  return (
+    <>
+      <h3 className='text-2xl capitalize ml-3 my-5 font-medium underline '>{categoryFilter}</h3>
+      <div className='ml-3'>
+      {filteredTests.map(test => (
+        <button className='border m-1 p-2 bg-btnbg text-black hover:bg-blue-500 font-medium hover:text-white rounded-xl'
+          key={test.testname}
+          id={test.testname}
+          type='submit'
+          onClick={(e) => onClick(e, test)}
+        >
+          {test.testtitle}
+        </button>
+      ))}
+      </div>
+    </>
+  )
+}
 
 function Test() {
 
   const history = useNavigate()
 
-  // a little inline styling
-  const style = {
-    margin: '10px',
-    padding: '5px'
-  }
-
   const [tests, setTests] = useState([])
-
 
   // TO GET CATEGORY WISE OBJECTS OF TEST
   function filterByCategory(objects, category) {
@@ -23,7 +43,7 @@ function Test() {
 
   const getAllTest = async () => {
     try {
-      const res = await fetch(ROOT+'/getnewtest', {
+      const res = await fetch(ROOT + '/getnewtest', {
         method: 'GET',
         headers: {
           // because there is cookies
@@ -41,7 +61,7 @@ function Test() {
     }
   }
 
-  
+
 
   const CollectUserData = (e, selectedtest) => {
     e.preventDefault()
@@ -56,36 +76,42 @@ function Test() {
 
 
   return (
-    <>
-      <h1>test</h1>
-      <br/>
-      <h3>Model Tests</h3>
-      {
-        tests.filter(test => test.category === 'modeltest').map(test => {
-          return <button id={test.testname} type='submits' style={style} onClick={(e) => CollectUserData(e, test)}>{test.testtitle}</button>
-        })
-      }
-      <br />
-      <h3>Daily Tests</h3>
-      {
-        tests.filter(test => test.category === 'dailytest').map(test => {
-          return <button id={test.testname} type='submits' style={style} onClick={(e) => CollectUserData(e, test)}>{test.testtitle}</button>
-        })
-      }
-      <h3>Weekly Tests</h3>
-      {
-        tests.filter(test => test.category === 'weeklytest').map(test => {
-          return <button id={test.testname} type='submits' style={style} onClick={(e) => CollectUserData(e, test)}>{test.testtitle}</button>
-        })
-      }
-      <h3>Archives</h3>
-      {
-        tests.filter(test => test.category == 'archive').map(test => {
-          return <button id={test.testname} type='submits' style={style} onClick={(e) => CollectUserData(e, test)}>{test.testtitle}</button>
-        })
-      }
-      <br />
-    </>
+    <div className='h-screen bg-pcolor text-white'>
+    <Header />
+      <TestButtonList tests={tests} categoryFilter='modeltest' onClick={CollectUserData} />
+      <TestButtonList tests={tests} categoryFilter='dailytest' onClick={CollectUserData} />
+      <TestButtonList tests={tests} categoryFilter='weeklytest' onClick={CollectUserData} />
+      <TestButtonList tests={tests} categoryFilter='archive' onClick={CollectUserData} />
+
+    </div>
+    // <div className='bg-pcolor text-white'>
+    //   <h3>Model Tests</h3>
+    //   {
+    //     tests.filter(test => test.category === 'modeltest').map(test => {
+    //       return <button id={test.testname} type='submits' style={style} onClick={(e) => CollectUserData(e, test)}>{test.testtitle}</button>
+    //     })
+    //   }
+    //   <br />
+    //   <h3>Daily Tests</h3>
+    //   {
+    //     tests.filter(test => test.category === 'dailytest').map(test => {
+    //       return <button id={test.testname} type='submits' style={style} onClick={(e) => CollectUserData(e, test)}>{test.testtitle}</button>
+    //     })
+    //   }
+    //   <h3>Weekly Tests</h3>
+    //   {
+    //     tests.filter(test => test.category === 'weeklytest').map(test => {
+    //       return <button id={test.testname} type='submits' style={style} onClick={(e) => CollectUserData(e, test)}>{test.testtitle}</button>
+    //     })
+    //   }
+    //   <h3>Archives</h3>
+    //   {
+    //     tests.filter(test => test.category == 'archive').map(test => {
+    //       return <button id={test.testname} type='submits' style={style} onClick={(e) => CollectUserData(e, test)}>{test.testtitle}</button>
+    //     })
+    //   }
+    //   <br />
+    // </div>
   )
 }
 
